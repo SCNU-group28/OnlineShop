debc37dff7e6f969ed24b787a4271320ae8c4916
root <root@Richard>
2023-10-08 19:00:10 +0800

    新增了不同URL对应的路由

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .apis import UserRegisterApi, UserLoginApi
-
+from .apis import UserRegisterApi, UserLoginApi, WXTrialApi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path("register/", UserRegisterApi.as_view(), name="register"),
     path("login/", UserLoginApi.as_view(), name="login"),
+    path("wxtrial/", WXTrialApi.as_view(), name="wx_trial"),
+    path("token_obtain/", TokenObtainPairView.as_view(), name="obtain"),
+    path("token_refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("token_verify/", TokenVerifyView.as_view(), name="verify"),
 ]
