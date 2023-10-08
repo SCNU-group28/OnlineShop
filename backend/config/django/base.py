d189c2acd5c2a8f6461e5806dfdec9b12c24b266
root <root@Richard>
2023-10-08 19:01:23 +0800

    新增应用postgresql数据库引擎

--- backend/config/django/base.py
+++ backend/config/django/base.py
@@ -141,13 +141,16 @@ else:
         raise Exception("DB_NAME, DB_USER, DB_PSWD not found in .env file")
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': "django.db.backends.postgresql",
+            'NAME': DB_NAME,
+            'USER': DB_USER,
+            'PASSWORD': DB_PSWD,
+            'HOST': DB_HOST,
+            'PORT': DB_PORT,
+        }
     }
-}
 
 
 # Password validation
