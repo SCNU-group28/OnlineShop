a6556fdfc8cf74e3ef7b4ab60dfe5e08c2bbfa98
root <root@Richard>
2023-10-12 10:17:16 +0800

    修改纠正无用路由

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -8,6 +8,5 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    # path("reports/", include("apps.crawler.urls"), name="reports"),
     path("account/", include("apps.account.urls"), name="account"),
 ]
